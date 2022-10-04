9b679f4979bf155b8d6dffee8a5b5ad54a1825f8
Jun <wnsgur4092@gmail.com>
2022-10-04 20:30:59 +1100

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by JunHyuk Lim on 4/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
